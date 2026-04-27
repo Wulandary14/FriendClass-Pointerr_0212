c9c4eada2b2f855a4b920b48861e4dcb457952a0
Wulandary14 <Moonywulan14@gmail.com>
2026-04-27 21:37:49 +0700

    masukkan diagonal 1

--- tugass.cpp
+++ tugass.cpp
@@ -7,3 +7,9 @@ private:
     int d1, d2;
     int a, b;
 
+public:
+    void inputData() {
+        cout << "\n=== Layang-Layang ===" << endl;
+        cout << "Masukkan diagonal 1 : ";
+        cin >> d1;
+       
